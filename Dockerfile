e1e4df75722b5fab18979dda299fe0044837e2f6
Jason Biegel <jbiegel@shoprunner.com>
2020-03-11 16:09:43 -0400

    wait script

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,13 @@
 FROM node:slim
 
+RUN apt-get update \
+    && apt-get install -y wget --no-install-recommends \
+    && rm -rf /var/lib/apt/lists/*
+
 WORKDIR /usr/home
 
 COPY index.js .
+COPY wait-until-unavailable .
 
 EXPOSE 3000
 
--- index.js
+++ index.js
@@ -8,27 +8,28 @@ const server = http.createServer((req,res) => {
 
     if(req.method !== 'GET') handleError(res, 405)
     
-    if(req.url === '/') {
-        res.writeHead(200, {'Content-Type' : 'text/html'});
-        res.statusCode = 200;
-        res.write(`<h1>Hello World</h1>`);
-        res.end();
-    }
-
     const {pathname, query} = url.parse(req.url);
 
-    if(pathname === '/hello') {
-        console.log('hello endpoint');
-        res.writeHead(200, {'Content-Type' : 'text/html'});
+    if(pathname === '/shutdown') {
+        console.log('shutdown endpoint');
         res.statusCode = 200;
-        const {name} = qs.parse(query);
-        res.write(`<h1>You sent the name: ${name}</h1>`);
+        res.end();
+        shutdown();
+    }
+    else if(pathname === '/healthy') {
+        console.log('health endpoint');
+        res.statusCode = 200;
+        res.write('ok');
         res.end();
     }
 
     handleError(res, 404);
         
 });
+
+function shutdown() {
+    server.close();
+}
 
 function handleError(res, code) {
     res.statusCode = code;
